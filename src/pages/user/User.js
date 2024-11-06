ff819f03abaf7245d4465a629c02d20d73256d93
Dattasai-git <113090102+Dattasai-git@users.noreply.github.com>
2024-11-05 19:12:33 -0500

    change coluer

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='text-white flex justify-center items-center w-full h-screen'>
+    <div className='text-blue flex justify-center items-center w-full h-screen'>
         <h1 className='text-4xl'>User page holder</h1>
     </div>
   )
